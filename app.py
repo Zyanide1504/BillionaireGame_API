21e59285bbf5b9cad46539f00160aa7cac89f0f3
Zyanide1504 <42516755+Zyanide1504@users.noreply.github.com>
2021-12-05 14:16:59 +0700

    add question id test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from  flask import  request
 
 app = Flask(__name__)
 
@@ -25,9 +26,12 @@ def hello():
     return "Hello Billionaire"
 
 
-@app.route('/api', methods=['GET'])
+@app.route('/get_question_by_id', methods=['GET'])
 def get_api():
-    return jsonify(data)
+    # if key doesn't exist, returns None
+    question_id = request.args.get('question_id')
+
+    return '''<h1>The question_id value is: {}</h1>'''.format(question_id)
 
 if __name__ == "__main__":
     app.run(debug=False)
