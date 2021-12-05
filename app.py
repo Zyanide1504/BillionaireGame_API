9c00579cdbc499e7a24fdda3977922d7bacaf13c
Zyanide1504 <42516755+Zyanide1504@users.noreply.github.com>
2021-12-05 14:05:01 +0700

    little Change

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-import flask
-from  flask import  request
+from flask import Flask, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def home():
-    arg = request.arges['arg1']
-    return  'Hello worlddd!'
+data = [
+        {
+            "id": 1,
+            "library": "Pandas",
+            "language": "Python"
+        },
+        {
+            "id": 2,
+            "library": "requests",
+            "language": "Python"
+        },
+        {
+            "id": 3,
+            "library": "NumPy",
+            "language": "Python"
+        }
+    ]
 
+@app.route('/')
+def hello():
+    return "Hello Flask-Heroku"
+
+
+@app.route('/api', methods=['GET'])
+def get_api():
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=False)
